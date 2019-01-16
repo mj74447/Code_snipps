06bca67e17eacfe4199ad0a5623ccaacd7286987
Mayank Joshi <mj74447@gmail.com>
2019-01-17 01:04:15 +0530

    Create EASY001.cpp

---
+++ codechef/EASY001.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+
+int main(int argc, char** argv) {
+	int n=0;
+	while(n!=42){
+		cin>>n;
+		if(n==42)
+		    break;
+		else
+		    cout<<n<<endl;
+	}
+
+	return 0;
+}
